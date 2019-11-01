6021b077be0ee816adbd0ba7a0d840267b57f8c4
GrantJMay <47539768+GrantJMay@users.noreply.github.com>
2019-10-31 18:58:09 -0700

    Add files via upload

--- simple_script.py
+++ simple_script.py
@@ -1,10 +1,13 @@
 #! /usr/bin/python3
 import os
 import json
+import cgi
+import cgitb
+import MySQLdb
 if "PATH_INFO" in os.environ:
     pathinfo = os.environ["PATH_INFO"]
     if pathinfo == "/jsontest":
-        print("Content-Type: application/json")
+        print("Content-type: application/json")
         print("Status: 200 OK")
         print()
         randomObject = [4, 17, None, {"a" : 0, "b" : 1, "c" : 2}, "Halloween", 33, "school"]
@@ -14,8 +17,19 @@ if "PATH_INFO" in os.environ:
         print("Content-Type: text/html")
         print("Status: 200 OK")
         print()
-        if pathinfo == "/correct":
-            print("Welcome to the secret page")
+        if pathinfo == "/form":
+            print('This is a form' )
+            form=cgi.FieldStorage()
+            sql_variable=form.getvalue('variable')
+            db = MySQLdb.connect('lab4.cixhqu4w47kx.us-east-1.rds.amazonaws.com', 'fakeuser', 'fakepassword', 'dogs')
+            cursor=db.cursor()
+            sql = "INSERT INTO dogs (name) VALUES (%s)"
+            cursor.execute(sql,sql_variable)
+            db.commit()
+            db.close()
+            new_id = cursor.lastrowid
+            cursor.close()
+            print(sql_variable)
         else:
             print("This is the default page")
             print("<br>")
